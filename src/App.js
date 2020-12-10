f180cc0c85c396954b0473ad702ecc0767ccb111
princy pk <princypkk@gmail.com>
2020-12-10 12:51:12 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+// import Amplify from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// import {AmplifySignOut,withAuthenticator} from '@aws-amplify/ui-react';
+// Amplify.configure(awsconfig);
+function App() {
+  return (
+    <div className="App">
+
+
+      <header className="App-header">
+      <h2>My app content</h2>
+       {/* <AmplifySignOut><h2>My app content</h2></AmplifySignOut> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
